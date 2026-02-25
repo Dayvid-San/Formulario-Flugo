035e637747e79e2644774f658fc8415a6197e3ba
dayvidcomerce <dayvidcomerce@gmail.com>
2026-02-25 21:57:02 +0000

    feat: tsconfig added

--- src/components/StepJobData.tsx
+++ src/components/StepJobData.tsx
@@ -4,9 +4,9 @@ import * as z from 'zod';
 import { TextField, Button, Box, Grid, Typography, MenuItem } from '@mui/material';
 import React from 'react';
 
+
 const jobSchema = z.object({
   cargo: z.string().min(1, "O cargo é obrigatório"),
-  salario: z.string().min(1, "O salário é obrigatório"),
 });
 
 type JobFormData = z.infer<typeof jobSchema>;
@@ -16,7 +16,6 @@ export const StepJobData = ({ onNext, onBack, data }: any) => {
     resolver: zodResolver(jobSchema),
     defaultValues: {
       cargo: data?.cargo || "",
-      salario: data?.salario || ""
     }
   });
 
@@ -27,14 +26,11 @@ export const StepJobData = ({ onNext, onBack, data }: any) => {
     }
   };
 
-  // Lista de cargos/departamentos para o Select
   const opcoesCargos = [
-    { value: "Desenvolvedor Front-end", label: "Desenvolvedor Front-end" },
-    { value: "Desenvolvedor Back-end", label: "Desenvolvedor Back-end" },
-    { value: "Desenvolvedor Full-stack", label: "Desenvolvedor Full-stack" },
-    { value: "Designer UI/UX", label: "Designer UI/UX" },
-    { value: "Recursos Humanos", label: "Recursos Humanos" },
-    { value: "Financeiro", label: "Financeiro" },
+    { value: "TI", label: "TI" },
+    { value: "Marketing", label: "Marketing" },
+    { value: "Design", label: "Design" },
+    { value: "Produto", label: "Produto" },
   ];
 
   return (
@@ -46,7 +42,6 @@ export const StepJobData = ({ onNext, onBack, data }: any) => {
       
       <Grid container spacing={4} sx={{ flexGrow: 1 }}>
         <Grid size={{ xs: 12 }}>
-          {/* Transformado em Select */}
           <TextField 
             select
             {...register('cargo')} 
@@ -57,12 +52,10 @@ export const StepJobData = ({ onNext, onBack, data }: any) => {
             InputLabelProps={{ shrink: true }} 
             sx={greenInputStyle}
           >
-            {/* Opção padrão desabilitada para forçar a escolha */}
             <MenuItem value="" disabled>
               <em>Selecione...</em>
             </MenuItem>
             
-            {/* Mapeando as opções para criar a lista */}
             {opcoesCargos.map((opcao) => (
               <MenuItem key={opcao.value} value={opcao.value}>
                 {opcao.label}
@@ -72,7 +65,6 @@ export const StepJobData = ({ onNext, onBack, data }: any) => {
         </Grid>
       </Grid>
 
-      {/* Botões - Voltar e Concluir */}
       <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 8 }}>
         <Button onClick={onBack} sx={{ color: '#9e9e9e', textTransform: 'none', fontWeight: 'bold' }}>
           Voltar
